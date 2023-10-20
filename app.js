ac7dce6e3ffec2f4604bb364ff8c43faa5848425
simpa007 <simpaimmey@gmail.com>
2023-10-20 12:14:03 -0700

    review

--- app.js
+++ app.js
@@ -422,6 +422,20 @@
 
 // console.log(removeDuplicate([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 
+function removeDuplicates(nums) {
+	if (nums.length === 0) {
+		return 0;
+	}
+	let index = 1;
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] !== nums[i + 1]) {
+			nums[index] = nums[i + 1];
+			index++;
+		}
+	}
+	return index;
+}
+console.log(removeDuplicates([1, 1, 2]));
 //output = [0,1,2,3,4,-,-,-,-,-];
 
 //SOLUTION 1: Squares of a Sorted Array
