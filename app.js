013e47cced5b944ec7a53dc1c5a4a20961c64174
simpa007 <simpaimmey@gmail.com>
2024-01-18 02:47:03 -0800

    updates

--- app.js
+++ app.js
@@ -1224,6 +1224,39 @@
 // };
 // console.log(singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]));
 
+//SEARCH A 2D MATRIX
+var searchMatrix = function (matrix, target) {
+	const [rows, cols] = [matrix.length, matrix[0].length];
+	let [left, right] = [0, rows * cols - 1];
+
+	while (left <= right) {
+		const mid = (left + right) >> 1;
+		const [row, col] = [Math.floor(mid / cols), mid % cols];
+		const guess = matrix[row][col];
+
+		const isTarget = guess === target;
+		if (isTarget) return true;
+
+		const isTargetGreater = guess < target;
+		if (isTargetGreater) left = mid + 1;
+
+		const isTargetLess = target < guess;
+		if (isTargetLess) right = mid - 1;
+	}
+
+	return false;
+};
+console.log(
+	searchMatrix(
+		[
+			[1, 3, 5, 7],
+			[10, 11, 16, 20],
+			[23, 30, 34, 60],
+		],
+		3
+	)
+);
+
 //SECTION 6 : OBJECT (HASH MAP)
 
 //FIND COUNT OF ALL PLAYERS
